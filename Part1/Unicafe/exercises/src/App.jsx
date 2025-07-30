6f057f7ab5cb053229a6f295a46d7728d0cf7b9b
Brett Spangler <49742551+brett-spangler@users.noreply.github.com>
2025-07-30 01:30:40 -0400

    Exercise 1.7 Completed

--- Part1/Unicafe/exercises/src/App.jsx
+++ Part1/Unicafe/exercises/src/App.jsx
@@ -26,6 +26,9 @@ const App = () => {
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <p>All: {good + neutral + bad}</p>
+        <p>Average: {(good - bad) / (good + neutral + bad || 1)}</p>
+        <p>Positive: {((good / (good + neutral + bad || 1)) * 100)} %</p>
       </div>
     </div>
   )
