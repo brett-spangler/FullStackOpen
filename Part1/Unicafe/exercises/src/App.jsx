c32e9654d441524374d0bbb69ed7de5cf6409858
Brett Spangler <49742551+brett-spangler@users.noreply.github.com>
2025-07-30 01:32:32 -0400

    Exercise 1.9 Completed

--- Part1/Unicafe/exercises/src/App.jsx
+++ Part1/Unicafe/exercises/src/App.jsx
@@ -4,6 +4,23 @@ const Button = ({ onClick, text }) => (
   <button onClick={onClick}>{text}</button>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {(good - bad) / total}</p>
+      <p>Positive: {((good / total) * 100).toFixed(1)} %</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -21,17 +38,8 @@ const App = () => {
       <Button onClick={handleGoodClick} text="good" />
       <Button onClick={handleNeutralClick} text="neutral" />
       <Button onClick={handleBadClick} text="bad" />
-      <div>
-        <h1>Statistics</h1>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>All: {good + neutral + bad}</p>
-        <p>Average: {(good - bad) / (good + neutral + bad || 1)}</p>
-        <p>Positive: {((good / (good + neutral + bad || 1)) * 100)} %</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
-
 export default App
